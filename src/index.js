7cfb05bead8db70edd5d7ab03ab7e16d53cdb963
tatsuya_oide <40861052+machamp0714@users.noreply.github.com>
2019-07-28 12:23:15 +0900

    変数と関数をJSXの中で使用する

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import { render } from "react-dom";
 
-var reactElement = <h2>こんにちは、世界</h2>;
+const title = "こんにちは、世界";
+const body = "こちらが本文です。";
+
+const returnStrings = () => {
+  return "文字列を返す";
+};
+
+var reactElement = (
+  <div>
+    <p>{returnStrings()}</p>
+    <p>{Math.random()}</p>
+    <h2>{title}</h2>
+    <p>{body}</p>
+  </div>
+);
 
 render(reactElement, document.getElementById("root"));
