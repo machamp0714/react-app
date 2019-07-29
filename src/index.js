5ed154f500b30dd88b10cf40ef1c83068bbce546
tatsuya_oide <40861052+machamp0714@users.noreply.github.com>
2019-07-29 23:40:17 +0900

    clickされた時にsetStateでstateを変更する

--- src/index.js
+++ src/index.js
@@ -10,14 +10,21 @@ class Human extends React.Component {
   // 必ずrenderメソッドを実装する必要がある
   render() {
     return (
+      // コールバック関数の中で使われるthisには注意が必要。
       <h2 onClick={this.onButtonClick}>
         {this.state.name} {this.props.age}
       </h2>
     );
   }
-  onButtonClick() {
-    alert("click");
-  }
+  // 一般的なメソッド定義だと、thisの参照先が意図したものではなくなってしまう。
+  // onBUttonClick() {
+  //   this.setState({name: "change"});
+  // }
+
+  // create-react-appで作られたReactアプリケーションでは、アロー関数を使うことで回避できる。
+  onButtonClick = () => {
+    this.setState({ name: this.state.name + "san" });
+  };
 }
 
 render(<Human age="30" />, document.getElementById("root"));
