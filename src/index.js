39e1ca6aa815d47589c9c9a9a2264843c44e1c43
tatsuya_oide <40861052+machamp0714@users.noreply.github.com>
2019-07-29 23:29:45 +0900

    React Class Componentの書き方

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import { render } from "react-dom";
-import { ReactComponent } from "./ReactComponent";
 
-render(
-  <ReactComponent name="nakanishi" music="jazz" />,
-  document.getElementById("root")
-);
+class Human extends React.Component {
+  constructor(props) {
+    // 必ず必要
+    super(props);
+    this.state = { name: "Oide" };
+  }
+  // 必ずrenderメソッドを実装する必要がある
+  render() {
+    return (
+      <h2 onClick={this.onButtonClick}>
+        {this.state.name} {this.props.age}
+      </h2>
+    );
+  }
+  onButtonClick() {
+    alert("click");
+  }
+}
+
+render(<Human age="30" />, document.getElementById("root"));
