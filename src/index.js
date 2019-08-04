1d85dbe017f428a493ef1343ba5cf9c86f9972b9
tatsuya_oide <40861052+machamp0714@users.noreply.github.com>
2019-08-04 10:53:07 +0900

    GiphyAPIを叩く

--- src/index.js
+++ src/index.js
@@ -5,40 +5,55 @@ import axios from "axios";
 
 // render(<TodoApp />, document.getElementById("root"));
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { member: [] };
-  }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { member: [] };
+//   }
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.getJson}>get json</button>
-        {this.memberList(this.state.member)}
-      </div>
-    );
-  }
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.getJson}>get json</button>
+//         {this.memberList(this.state.member)}
+//       </div>
+//     );
+//   }
 
-  memberList = list => {
-    const memberList = list.map((member, index) => {
-      return (
-        <li>
-          {index} {member.name} {member.age}
-        </li>
-      );
-    });
+//   memberList = list => {
+//     const memberList = list.map((member, index) => {
+//       return (
+//         <li>
+//           {index} {member.name} {member.age}
+//         </li>
+//       );
+//     });
 
-    return <ul>{memberList}</ul>;
-  };
+//     return <ul>{memberList}</ul>;
+//   };
 
-  getJson = () => {
-    const url = "https://api.myjson.com/bins/6bjx9";
+//   getJson = () => {
+//     const url = "https://api.myjson.com/bins/6bjx9";
 
-    axios.get(url).then(res => {
-      this.setState(res.data);
-    });
-  };
-}
+//     axios.get(url).then(res => {
+//       this.setState(res.data);
+//     });
+//   };
+// }
 
-render(<App />, document.getElementById("root"));
+// render(<App />, document.getElementById("root"));
+
+const search = "cat";
+const key = "BlLVN1kAzi2C5fqMynCUpbxWhqa6qOxL";
+const limit = 3;
+
+const url = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${key}&limit=${limit}`;
+
+axios.get(url).then(res => {
+  const data = res.data.data;
+  const imageURL = data[0].images.downsized.url;
+  const img = document.createElement("img");
+  img.src = imageURL;
+
+  document.body.appendChild(img);
+});
