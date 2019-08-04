1eaf31a05ecdcb60a2498014b04f9bc5f31d1aa3
tatsuya_oide <40861052+machamp0714@users.noreply.github.com>
2019-08-04 10:33:33 +0900

    JSONを取得する

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 import React from "react";
 import { render } from "react-dom";
-import { TodoApp } from "./TodoApp";
+// import { TodoApp } from "./TodoApp";
+import axios from "axios";
 
-render(<TodoApp />, document.getElementById("root"));
+// render(<TodoApp />, document.getElementById("root"));
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { member: [] };
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.getJson}>get json</button>
+        {this.memberList(this.state.member)}
+      </div>
+    );
+  }
+
+  memberList = list => {
+    const memberList = list.map((member, index) => {
+      return (
+        <li>
+          {index} {member.name} {member.age}
+        </li>
+      );
+    });
+
+    return <ul>{memberList}</ul>;
+  };
+
+  getJson = () => {
+    const url = "https://api.myjson.com/bins/6bjx9";
+
+    axios.get(url).then(res => {
+      this.setState(res.data);
+    });
+  };
+}
+
+render(<App />, document.getElementById("root"));
