d247c0e6ec028c203c5fbc77c2c627d7c13df4f6
tatsuya_oide <40861052+machamp0714@users.noreply.github.com>
2019-08-07 00:29:08 +0900

    createStoreでstoreを作る

--- src/index.js
+++ src/index.js
@@ -1,92 +1,117 @@
-import React from "react";
-import { render } from "react-dom";
-// import { TodoApp } from "./TodoApp";
-import axios from "axios";
-import { Search } from "./Components/Search";
-import "./App.css";
+// import React from "react";
+// import { render } from "react-dom";
+// // import { TodoApp } from "./TodoApp";
+// import axios from "axios";
+// import { Search } from "./Components/Search";
+// import "./App.css";
 
-// render(<TodoApp />, document.getElementById("root"));
+// // render(<TodoApp />, document.getElementById("root"));
+
+// // class App extends React.Component {
+// //   constructor(props) {
+// //     super(props);
+// //     this.state = { member: [] };
+// //   }
+
+// //   render() {
+// //     return (
+// //       <div>
+// //         <button onClick={this.getJson}>get json</button>
+// //         {this.memberList(this.state.member)}
+// //       </div>
+// //     );
+// //   }
+
+// //   memberList = list => {
+// //     const memberList = list.map((member, index) => {
+// //       return (
+// //         <li>
+// //           {index} {member.name} {member.age}
+// //         </li>
+// //       );
+// //     });
+
+// //     return <ul>{memberList}</ul>;
+// //   };
+
+// //   getJson = () => {
+// //     const url = "https://api.myjson.com/bins/6bjx9";
+
+// //     axios.get(url).then(res => {
+// //       this.setState(res.data);
+// //     });
+// //   };
+// // }
+
+// // render(<App />, document.getElementById("root"));
 
 // class App extends React.Component {
 //   constructor(props) {
 //     super(props);
-//     this.state = { member: [] };
+//     this.state = { gifUrlList: [] };
 //   }
 
 //   render() {
 //     return (
 //       <div>
-//         <button onClick={this.getJson}>get json</button>
-//         {this.memberList(this.state.member)}
+//         <Search search={this.giphyApi} />
+//         {this.renderImgaeList(this.state.gifUrlList)}
 //       </div>
 //     );
 //   }
 
-//   memberList = list => {
-//     const memberList = list.map((member, index) => {
+//   renderImgaeList(list) {
+//     const imageList = list.map(url => {
+//       // 必ず閉じタグが必要！！
 //       return (
-//         <li>
-//           {index} {member.name} {member.age}
+//         <li className="item">
+//           <img className="image" src={url} alt="猫画像" />
 //         </li>
 //       );
 //     });
 
-//     return <ul>{memberList}</ul>;
-//   };
+//     return <ul className="list">{imageList}</ul>;
+//   }
 
-//   getJson = () => {
-//     const url = "https://api.myjson.com/bins/6bjx9";
+//   giphyApi = query => {
+//     const q = query;
+//     const key = "BlLVN1kAzi2C5fqMynCUpbxWhqa6qOxL";
+//     const limit = 30;
+//     const url = `https://api.giphy.com/v1/gifs/search?q=${q}&api_key=${key}&limit=${limit}`;
 
 //     axios.get(url).then(res => {
-//       this.setState(res.data);
+//       const data = res.data.data;
+//       const imageUrlList = data.map(e => {
+//         return e.images.downsized.url;
+//       });
+//       this.setState({ gifUrlList: imageUrlList });
 //     });
 //   };
 // }
 
 // render(<App />, document.getElementById("root"));
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { gifUrlList: [] };
+import { createStore } from "redux";
+
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "PLUS_ONE":
+      return state + 1;
+    case "MINUS_ONE":
+      return state - 1;
+    default:
+      return state;
   }
+};
 
-  render() {
-    return (
-      <div>
-        <Search search={this.giphyApi} />
-        {this.renderImgaeList(this.state.gifUrlList)}
-      </div>
-    );
-  }
+const store = createStore(reducer);
 
-  renderImgaeList(list) {
-    const imageList = list.map(url => {
-      // 必ず閉じタグが必要！！
-      return (
-        <li className="item">
-          <img className="image" src={url} alt="猫画像" />
-        </li>
-      );
-    });
+console.log(store);
 
-    return <ul className="list">{imageList}</ul>;
-  }
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
-  giphyApi = query => {
-    const q = query;
-    const key = "BlLVN1kAzi2C5fqMynCUpbxWhqa6qOxL";
-    const limit = 30;
-    const url = `https://api.giphy.com/v1/gifs/search?q=${q}&api_key=${key}&limit=${limit}`;
-
-    axios.get(url).then(res => {
-      const data = res.data.data;
-      const imageUrlList = data.map(e => {
-        return e.images.downsized.url;
-      });
-      this.setState({ gifUrlList: imageUrlList });
-    });
-  };
-}
-
-render(<App />, document.getElementById("root"));
+store.dispatch({ type: "PLUS_ONE" });
+store.dispatch({ type: "MINUS_ONE" });
+store.dispatch({ type: "PLUS_ONE" });
